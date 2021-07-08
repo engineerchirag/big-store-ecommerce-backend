78f369646e4f50247ecbd5f4aabc6c84a5f4780c
engineerchirag <engineerchirag021@gmail.com>
2021-07-08 22:11:00 +0530

    Athentication & authorization

--- src/middleware/authentication.js
+++ src/middleware/authentication.js
@@ -1,11 +1,39 @@
 exports.authentication = function(req, res, next) {
     // Check if auth token is present in header
-    // TODO: Check if token is valid
     var authToken = req.header('Authorization');
-    if (authToken) {
+    // TODO: Check if token is valid in DB or using jwt controller
+    var userDetail = {
+      id: 1,
+      firstName: 'Chirag',
+      roles: ['ADMIN'],
+    }; // Do a lookup from DB validateTokenAndGetUserDetail(authToken);
+    // throw new Error('Failed to fetch data from DB'); // Global uncaught Error handling 
+    if (userDetail) {
+      req.user = userDetail;
+      console.log('authentication');
       next();
     } else {
       res.send({
-        message: 'Unauthorised request',
+        message: 'Unauthentic request',
     });
 }};
+
+exports.authorization = function(roles) {
+  return function(req, res, next) {
+    try {
+      const haspMap = new Set(req.user.roles);
+      roles.forEach(role => {
+          if (haspMap.has(role)){
+            next();
+          }
+      });
+      res.send({
+        message: 'Unauthorised request',
+      });
+    } catch(e) {
+      res.send({
+        message: 'Something went wrong',
+      });
+    }
+  };
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var { authorization } = require('../middleware/authentication'); 
 
 /* GET home page. */
-router.get('/list', function(req, res, next) {
+router.get('/list', authorization(['ADMIN']), function(req, res, next) {
   res.send([{ "title": 'Product 1'} , {"title": 'Product 2' }]);
 });
 
-router.get('/', function(req, res, next) {
+router.get('/', authorization(['ADMIN', 'ANALYST']), function(req, res, next) {
   res.send({ "title": 'Express' });
 });
 
